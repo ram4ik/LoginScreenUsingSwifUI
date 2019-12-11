4f08d88491b0f7020ace8a2a7c829286a9d9b64d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-11 20:40:28 +0200

    Initial Commit

---
+++ LoginScreenUsingSwifUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginScreenUsingSwifUI
+//
+//  Created by Ramill Ibragimov on 11.12.2019.
+//  Copyright © 2019 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
